0093a0d69e947c0ea9c99159088f4865c1f1553b
XuyenPT <83726468+XuyenPT@users.noreply.github.com>
2021-05-10 23:39:31 +0700

    Add files via upload

---
+++ java/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Rectangle rec = new Rectangle();
+        rec.chuvi();
+        rec.dientich();
+    }
+}
